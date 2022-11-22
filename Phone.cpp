7e61b7406999e28b92d3c5a4c3f3d6a8d68d5599
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-22 11:14:34 +0300

    Add files via upload

---
+++ Phone.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, t, pay;
+    cin >> a >> b >> c >> t;
+
+    if (a <= t)
+    {
+        pay = t * b;
+    }
+    if (a > t)
+    {
+        pay = c * t;
+    }
+    cout << pay;
+    system("pause");
+}
